36547818324be92affb5bc46794382d203be124f
Dorian Kcira <dkcira@gmail.com>
2021-09-10 09:47:56 -0700

    run local and new version

---
+++ unicodex/buildrunlocal.ps1
@@ -0,0 +1,15 @@
+
+# dk 20210910
+
+# print out commands before executing them
+Set-PSDebug -Trace 1
+
+# build
+docker-compose build
+
+# migrate and load sample data
+docker-compose run --rm web python manage.py migrate
+docker-compose run --rm web python manage.py loaddata sampledata
+
+# start dockers in background (detached mode)
+docker-compose up -d
